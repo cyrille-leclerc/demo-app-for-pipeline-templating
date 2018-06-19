8589c66c084dd5d627db019130fdf51fc2d0a286
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-06-19 22:42:34 +0200

    initial commit

---
+++ src/main/java/com/example/templating/demoappforpipelinetemplating/DemoAppForPipelineTemplatingApplication.java
@@ -0,0 +1,12 @@
+package com.example.templating.demoappforpipelinetemplating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppForPipelineTemplatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppForPipelineTemplatingApplication.class, args);
+	}
+}
